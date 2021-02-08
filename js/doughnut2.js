d0d2a905298c58186782952e806cb60fe44dcea3
furatmalkawi29 <malkawifurat29@gmail.com>
2021-02-08 13:01:15 +0200

    done

---
+++ js/doughnut2.js
@@ -0,0 +1,37 @@
+
+   
+       var DoughTyp = prompt("Pink or Yellow Glazed Doughnut?", 'pink');
+   
+       while(DoughTyp !== "pink" && DoughTyp !== "yellow")
+       {
+           DoughTyp = prompt ('Pink or Yellow Glazed Doughnut?');
+           
+       }
+   
+       var pickedItem = '';
+       
+       if (DoughTyp == "pink")
+       {
+           
+               pickedItem = '<img src="pinkk.png" style="width:200px">';
+           
+   
+       } else if (DoughTyp == "yellow")
+       {
+        
+               pickedItem = '<img src="yelloww.png" style="width:230px">';
+       }
+   
+       var DoughNum = prompt("How Many Doughnut?",'1');
+   
+       var picturesArray = '';
+   
+       for ( i=0; i<DoughNum ; i++ )
+       {
+   picturesArray= picturesArray + pickedItem;
+       }
+   
+       document.write(picturesArray);
+   
+
+
